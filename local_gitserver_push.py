2b671e8b24514a5234278da7f1efee2a79f1d547
root <root@test.net>
2017-04-24 21:31:47 +0200

    v0.1

--- local_gitserver_push.py
+++ local_gitserver_push.py
@@ -2,11 +2,11 @@
 ## For pulling from local git server (HTTP)
 ##
 from git import Repo, repo
-import git
 import os, shutil
+#import os.path as osp
 
 try:
-   username = raw_input("Username [defaul=git]: ") or "git"
+   username = raw_input("Username [default=git]: ") or "git"
    workdir = raw_input("Workdir [default=/tmp]: ") or "/tmp"
    git_server = raw_input("Git Server [default=127.0.0.1]: ") or "127.0.0.1"
    project = raw_input("Projectname: ")
